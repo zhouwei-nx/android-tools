f9603ce5d187bd7d4acc1909317c0a51f1c7b5b6
1300012858 <1300012858@qq.com>
2022-09-02 12:18:58 +0800

    3

---
+++ app/src/main/java/com/bjyx/android_tools/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bjyx.android_tools;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
